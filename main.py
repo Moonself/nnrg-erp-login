53d8de1b010181d479f582f601c8ec510b6a0885
Moonself <134387747+Moonself@users.noreply.github.com>
2024-01-25 20:14:28 +0530

    Update main.py
    match works on python 3.10
    most have 3.8

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ print("Options  are \n1.login\n2.Get attendance\n3.Get marks\n4.Get both marks a
 choice = None
 while (choice != 8):
     choice  = input('Enter your choice: ')
-    match choice:
-        case '1':
-            new_session.login(input('Enter Roll No. :'))
-        case '2':
-            new_session.get_atd()
-        case '3':
-            new_session.get_marks(True,True)
-        case '4':
-            new_session.get_atd()
-            new_session.get_marks(False,False)
-        case '5':
-            choice = 8
+    if choice == '1':
+        new_session.login(input('Enter Roll No. :'))
+    elif choice == '2':
+        new_session.get_atd()
+    elif choice == '3':
+        new_session.get_marks(True,True)
+    elif choice == '4':
+        new_session.get_atd()
+        new_session.get_marks(False,False)
+    elif choice == '5':
+        choice = 8
 
